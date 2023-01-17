e03802ebdb9fd2ab7e2a6ff87b52edf39e7cb90e
Rafael Tomas <rafaeltomassouza@gmail.com>
2023-01-17 01:04:59 -0300

    feat: create page login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './access/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, NgbModule],
+  declarations: [AppComponent, LoginComponent],
+  imports: [BrowserModule, NgbModule, AppRoutingModule,],
   providers: [],
   bootstrap: [AppComponent],
 })
