f1df9099d3b03cbacda2e07bb3529eda824310d4
liedsonft <liedsonalves066@gmail.com>
2023-01-20 16:49:39 -0300

    refactor: add dashboard route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,13 @@ import { AdminComponent } from './admin/admin.component';
 import { AppComponent } from './app.component';
 import { AuthGuard } from './access/auth/auth.guard';
 import { PatientComponent } from './patient/patient.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
   {
     path: 'admin',
     component: AdminComponent,
