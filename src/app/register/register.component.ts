4ba54c2e5a0141176c47f440b927cc4880d659cf
0Guilhermino <angeloguilhermino@gmail.com>
2023-01-20 01:30:48 -0300

    refactor(register): implementation angular material, and increments partial itens this form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+interface Cargo {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+
+  cargos: Cargo[] = [
+    {value: 'doctor', viewValue: 'Médico(a)'},
+    {value: 'nurse', viewValue: 'Enfermeiro(a)'},
+    {value: 'nurse_technician', viewValue: 'Téc. Enf.'},
+  ];
 
 }
