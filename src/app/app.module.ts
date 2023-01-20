29e76531c5ea44f5f595e47a09cea063aa033ae8
0Guilhermino <angeloguilhermino@gmail.com>
2023-01-20 14:52:26 -0300

    refactor(register): add inputfields and buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input'
 import {MatSelectModule} from '@angular/material/select';
 import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatButtonModule } from  '@angular/material/button';
 
 
 @NgModule({
@@ -34,7 +35,8 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
     MatInputModule,
     MatCardModule,
     MatSelectModule,
-    MatDatepickerModule
+    MatDatepickerModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
