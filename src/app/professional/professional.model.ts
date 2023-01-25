70177796fd5f3fe1091ee2c6e4a6dfd3f67e023f
Angelo Guilhermino <angeloguilhermino@gmail.com>
2023-01-24 22:47:06 -0300

    refactor(professional-create): add professional.model

---
+++ src/app/professional/professional.model.ts
@@ -0,0 +1,19 @@
+export interface Professional {
+    id?: number
+    name: string
+    cpf: Date
+    dateOfBirth: Date
+    phone: string
+    email: string
+    gender: string
+    maritalStatus: string
+    crm: string
+    specialty: string
+    zipcode: string
+    street: string
+    number: string
+    district: string
+    city: string
+    state: string
+    complements: string
+}
