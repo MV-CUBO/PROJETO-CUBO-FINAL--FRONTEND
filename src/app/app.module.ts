8fe6b440b2cbc1202ffafd14f4d8bf269e9cc4f2
liedsonft <liedsonalves066@gmail.com>
2023-01-29 19:40:03 -0300

    refactor: refactoring the modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,22 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BodyComponent } from './body/body.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { LayoutModule } from '@angular/cdk/layout';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginModule } from './access/login/login.module';
-import { NgChartsModule, NgChartsConfiguration } from 'ng2-charts'
 import { PepCreateComponent } from './pep/pep-create/pep-create.component';
 import { PepModule } from './pep/pep.module';
 import { AuthInterceptor } from './access/auth/authInterceptor';
 import { PatientModule } from './patient/patient.module';
-import { ProfessionalModule } from './professional/professional.module';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 
 @NgModule({
@@ -30,27 +22,17 @@ import { MAT_DATE_LOCALE } from '@angular/material/core';
     AppComponent,
     BodyComponent,
     SidenavComponent,
-    DashboardComponent,
     PepCreateComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    MatGridListModule,
-    MatCardModule,
-    MatMenuModule,
-    MatIconModule,
-    MatButtonModule,
-    LayoutModule,
     HttpClientModule,
     LoginModule,
-    NgChartsModule,
     PepModule,
     PatientModule,
-    ProfessionalModule
-    
-
+    DashboardModule
   ],
   
   providers: [
@@ -60,7 +42,6 @@ import { MAT_DATE_LOCALE } from '@angular/material/core';
       multi: true
     },
     { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
-    { provide: NgChartsConfiguration, useValue: { generateColors: false }}
   ],
   bootstrap: [AppComponent]
 })
